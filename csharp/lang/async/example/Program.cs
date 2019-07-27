fc59ed2f6b6bb9825db13dbf14718ed09149888a
Sam Loescher <samuel.loescher@gmail.com>
2019-07-27 15:34:15 +0100

    Add c# async example

---
+++ csharp/lang/async/example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var asyncTask = DispatchAsync();
+        DoSyncWork();
+        asyncTask.Wait(); // Blocking call, used on the main thread
+        Console.WriteLine("Async task complete? " + asyncTask.IsCompleted);
+    }
+
+    public static void DoSyncWork()
+    {
+        Console.WriteLine("Doing sync task before async completes");
+    }
+    public static async Task DispatchAsync()
+    {
+        await LongRunningFunction('a');
+        LongRunningFunction('b'); // Forget to await! CS4014
+    }
+
+    public static async Task LongRunningFunction(char c)
+    {
+        Console.WriteLine("My Long Running Function Start : " + c);
+        await Task.Delay(1000);
+        Console.WriteLine("My Long Running Function End : " + c);
+    }
+}
